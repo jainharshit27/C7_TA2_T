3b405ced0f226798cfb3a155ebfbf5e40c12c293
jainharshit27 <82932932+jainharshit27@users.noreply.github.com>
2021-07-10 08:12:38 +0530

    Add files via upload

---
+++ C7 TA2 reference.py
@@ -0,0 +1,15 @@
+# WHILE LOOP
+import random
+ring = random.randint(1,12)
+print(ring)
+
+i = 1
+while i<=12:
+    if i == ring:
+        print(i)
+        break
+    else:
+        print(i)
+        i+=1
+        
+     
